9d3ca9ff065b041ed2d0f346c0e1622ad39cc361
Breno Almeida <breno.almeida@cmtecnologia.com.br>
2023-08-20 20:15:38 -0300

    [Feature]: Movment

--- src/client/models/Player.ts
+++ src/client/models/Player.ts
@@ -2,14 +2,16 @@ import { Attributes, Stats } from "../../Types"
 import Sprite from "./Sprite.js";
 export default class Player {
     public stats: Stats;
-    public attributes: Attributes
+    public attributes: Attributes;
     public skill_points: number;
     public sprite: Sprite;
-    constructor(stats: Stats, attributes: Attributes, skill_points: number, sprite: Sprite, fillStyle: string) {
+    public id: string;
+    constructor(stats: Stats, attributes: Attributes, skill_points: number, sprite: Sprite, id: string) {
+        this.id = id;
         this.attributes = attributes;
         this.stats = stats;
         this.skill_points = skill_points;
-        this.sprite = new Sprite(sprite.position, sprite.velocity, sprite.height, sprite.width, fillStyle);
+        this.sprite = new Sprite(sprite.position, sprite.height, sprite.width);
     }
 
 } 
