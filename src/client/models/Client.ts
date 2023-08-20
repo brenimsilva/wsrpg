7954784c1ef65d5c6141850b206d2e90c58f6643
Breno Almeida <breno.almeida@cmtecnologia.com.br>
2023-08-20 20:14:41 -0300

    [feature]: movment

--- src/client/models/Client.ts
+++ src/client/models/Client.ts
@@ -1,7 +1,6 @@
 import Player from "./Player.js";
 import { ConMessage, DesConMessage, IMessage } from "../../Types";
 import { start } from "../Game.js";
-import { ClientSocket, bundleMessage } from "../../server/server.js";
 
 export type TClient = {
     id: string,
@@ -35,17 +34,17 @@ export default class Client extends WebSocket{
                     const msgConverted: TClient[] = msgJson.message as unknown as TClient[]
                     const players: Array<Player> = msgConverted.map(c => {
                         const fillColor = c.id === this._id ? "blue" : "green"
-                        return  new Player(c.player.stats, c.player.attributes, c.player.skill_points, c.player.sprite, fillColor);
-                    })
+                        return  new Player(c.player.stats, c.player.attributes, c.player.skill_points, c.player.sprite, fillColor, c.id);
+                    });
+                    const playerIndex = players.findIndex(p => p.id === this._id);
                     
                     console.log(players)
-                    start(players, this._player);
+                    start(players, playerIndex);
                     break;
                 }
                 case "CON": {
                     const json: TClient = JSON.parse(msg.data).message.client;
                     this._id = json.id;
-                    console.log(this._id)
                     console.log("ID atribuido com sucesso");
                     break;
                 }
