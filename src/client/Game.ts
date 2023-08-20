ccfa29bb3f2333b68955c76c1f7cfa889bfa985e
Breno Almeida <breno.almeida@cmtecnologia.com.br>
2023-08-20 20:13:20 -0300

    Movment

--- src/client/Game.ts
+++ src/client/Game.ts
@@ -5,13 +5,39 @@ import { ctx, canvas } from "./models/Canvas.js";
 import Player from "./models/Player.js";
 import { ClientSocket } from "../server/server.js";
 
-export function start(players: Array<Player>, player: Player) {
-    window.requestAnimationFrame(() => start(players, player));
-    ctx.fillStyle = "black"
-    ctx.fillRect(0,0, canvas.width, canvas.height);
-    players.forEach(p => {
-        p.sprite.update();
+export function start(players: Array<Player>, playerIndex: number) {
+    window.addEventListener("keydown", ({key}) => {
+        switch(key) {
+            case "a":
+                players[playerIndex].sprite.velocity.x = -5;
+            break;
+
+            case "d":
+                players[playerIndex].sprite.velocity.x = 5;
+            break;
+        }
     })
+
+    window.addEventListener("keyup", ({key}) => {
+        switch(key) {
+            case "a":
+                players[playerIndex].sprite.velocity.x = 0;
+            break;
+            case "d":
+                players[playerIndex].sprite.velocity.x = 0;
+            break;
+        }
+    })
+
+    animate();
+    function animate() {
+        window.requestAnimationFrame(animate);
+        ctx.fillStyle = "black"
+        ctx.fillRect(0,0, canvas.width, canvas.height);
+        players.forEach(p => {
+            p.sprite.update();
+        })
+    }
 }
 
 
